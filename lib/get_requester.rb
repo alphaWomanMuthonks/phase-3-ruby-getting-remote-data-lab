5d769e4608f8f72dbda857f7598f1e3235ee65c5
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-11-08 10:14:47 +0300

    using parse json and get request

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,23 @@
 # Write your code here
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+class GetRequester
+
+    attr_accessor :url 
+
+    def initialize(url)
+        @url = url 
+    end
+
+    def get_response_body
+        uri = URI.parse(url)
+        response = NET::HTTP.get_response_body(url)
+        response.body
+    end
+
+    def parse_json
+        response=JSON.parse(self.get_response_body)
+    end
+end
